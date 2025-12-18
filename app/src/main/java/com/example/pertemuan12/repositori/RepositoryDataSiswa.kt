853c4bb4d597a837912d92b2889846246f5a9dd2
Lalu Fatan <lalufatan25@gmail.com>
2025-12-18 16:34:24 +0700

    menambhkan code repositoryDataSiswa

--- app/src/main/java/com/example/pertemuan12/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/repositori/RepositoryDataSiswa.kt
@@ -1,3 +1,24 @@
 package com.example.pertemuan12.repositori
 
-int
+import com.example.pertemuan12.apiservice.ServiceApiSiswa
+import com.example.pertemuan12.modeldata.DataSiswa
+
+interface RepositoryDataSiswa {
+
+    suspend fun getDataSiswa() : List<DataSiswa>
+
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+//  suspend fun getStatusSiswa(id:Int) : DataSiswa
+//  suspend fun editDataSiswa(id:Int,DataSiswa) :retrofit2.Response<Void>
+//  suspend fun hapusDataSiswa(id: Int) :retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+//override suspend fun getSatuSiswa(id: Int): DataSiswa - serviceApiSiswa.getSatuSiswa()
+//override suspend fun ediSatuSiswa(id: Int,dataSiswa :DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.editSiswa(id,dataSiswa)
+//override suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void> = serviceApiSiswa.hapusSatuSiswa(id)
+}
