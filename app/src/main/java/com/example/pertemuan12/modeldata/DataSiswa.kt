7a6d277b1cf5149e69a5eb4381bd7ebd58a2a055
Lalu Fatan <lalufatan25@gmail.com>
2025-12-18 15:53:05 +0700

    menambhkan dun di data siswa

--- app/src/main/java/com/example/pertemuan12/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/modeldata/DataSiswa.kt
@@ -1,2 +1,30 @@
 package com.example.pertemuan12.modeldata
 
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+
+data class  DataSiswa(
+    val id :Int,
+    val nama : String,
+    val alamat: String,
+    val telpon : String,
+)
+data class  UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+data class  DetailSiswa(
+    val id :Int = 0,
+    val nama : String = "",
+    val alamat: String = "",
+    val telpon : String= "",
+)
+
+fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
