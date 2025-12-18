3ca7232d5a48d8a90cf0e072c0857cd858416213
Lalu Fatan <lalufatan25@gmail.com>
2025-12-18 21:45:32 +0700

    menambhkan code di HomeViewModel.kt

--- app/src/main/java/com/example/pertemuan12/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/HomeViewModel.kt
@@ -1,2 +1,44 @@
 package com.example.pertemuan12.viewmodel
 
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan12.model.DataSiswa
+import com.example.pertemuan12.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<DataSiswa>) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
+
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            listSiswa = try {
+
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            } catch (e: HttpException) {
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
