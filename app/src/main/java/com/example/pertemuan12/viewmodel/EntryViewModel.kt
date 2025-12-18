3bf5cdbb71e6a79362d1678bd71814cf2ba73d6d
Lalu Fatan <lalufatan25@gmail.com>
2025-12-18 21:44:10 +0700

    menambhkan code di EntryViewModel.kt

--- app/src/main/java/com/example/pertemuan12/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/EntryViewModel.kt
@@ -1,4 +1,43 @@
 package com.example.pertemuan12.viewmodel
 
-class EntryViewModel {
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan12.model.DetailSiswa
+import com.example.pertemuan12.model.UIStateSiswa
+import com.example.pertemuan12.model.toDataSiswa
+import com.example.pertemuan12.repositori.RepositoryDataSiswa
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            try {
+
+                repositoryDataSiswa.insertSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+                println("Sukses Tambah Data")
+            } catch (e: Exception) {
+                println("Gagal tambah data: ${e.message}")
+            }
+        }
+    }
 }
