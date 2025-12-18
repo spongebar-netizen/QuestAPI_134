6fe868b5b367197958e37bb6b9aff4bfa6d73efa
Lalu Fatan <lalufatan25@gmail.com>
2025-12-18 22:13:49 +0700

    mememperbaiki HomeViewModel.kt

--- app/src/main/java/com/example/pertemuan12/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/HomeViewModel.kt
@@ -6,7 +6,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.pertemuan12.model.DataSiswa
+import com.example.pertemuan12.modeldata.DataSiswa
 import com.example.pertemuan12.repositori.RepositoryDataSiswa
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
