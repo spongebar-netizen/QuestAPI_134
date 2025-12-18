48001eafcb90f3b1fd9e451989dbb0cb02a4def4
Lalu Fatan <lalufatan25@gmail.com>
2025-12-18 21:43:36 +0700

    menambhkan code di DestinasiHome.kt

--- app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiHome.kt
+++ app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiHome.kt
@@ -1,4 +1,9 @@
 package com.example.pertemuan12.uicontroller.route
 
-object DestinasiHome {
+
+import com.example.pertemuan12.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
