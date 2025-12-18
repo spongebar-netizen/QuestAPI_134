fe41067890dc0b5aaac1ddfa7fd0403e5a11a81a
Lalu Fatan <lalufatan25@gmail.com>
2025-12-18 21:20:53 +0700

    menambhkan code di destinasi entry

--- app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiEntry.kt
@@ -1,4 +1,9 @@
 package com.example.pertemuan12.uicontroller.route
 
-object DestinasiEntry {
+
+import com.example.mydatasiswa.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }
