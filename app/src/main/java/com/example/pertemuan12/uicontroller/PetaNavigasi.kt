3ddb6da8d207e0828edb9a478d959f7f87c22d45
Lalu Fatan <lalufatan25@gmail.com>
2025-12-18 21:46:24 +0700

    menambhkan code di PetaNavigasi.kt

--- app/src/main/java/com/example/pertemuan12/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/pertemuan12/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,45 @@
 package com.example.pertemuan12.uicontroller
 
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan12.uicontroller.route.DestinasiEntry
+import com.example.pertemuan12.uicontroller.route.DestinasiHome
+import com.example.pertemuan12.view.HomeScreen
+import com.example.pertemuan12.view.EntrySiswaScreen
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier = Modifier) {
+    HostNavigasi(navController = navController, modifier = modifier)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = {
+                    // Kode navigasi ke detail (sesuai gambar, bagian ini di-komen/belum ada)
+                    // navController.navigate("${DestinasiDetail.route}/$it")
+                }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
