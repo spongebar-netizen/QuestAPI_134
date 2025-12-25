119143ef81fb277b7029938bc8f46fbcdfdc6453
Lalu Fatan <lalufatan25@gmail.com>
2025-12-25 19:42:51 +0700

    menambahkan kode di ServiceApiSiswa.kt

--- app/src/main/java/com/example/pertemuan12/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/apiservice/ServiceApiSiswa.kt
@@ -2,8 +2,11 @@ package com.example.pertemuan12.apiservice
 
 import com.example.pertemuan12.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
@@ -12,12 +15,12 @@ interface ServiceApiSiswa {
     @POST ("InsertTM.php")
     suspend fun  postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 
-    //@GET("bacaTeman.php/{id}")
-    //suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
-    //
-    //@PUT("editTM.php/{id}")
-    //suspend fun editSatuSiswa(@Query("id") id:Int,@Body dataSiswa):retrofit2.Response<Void>
-    //
-    //@DELETE("deleteTM.php/{id}")
-    //suspend fun hapusSatuSiswa(@Query("id") id: Int):retrofit2.Response<Void>
+    @GET("bacaTeman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id:Int,@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id") id: Int):retrofit2.Response<Void>
 }
