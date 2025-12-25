6269a8a721e9120f24ddeb7a964e1a5d4d5f8749
Lalu Fatan <lalufatan25@gmail.com>
2025-12-25 22:12:25 +0700

    menambahkan kode EditViewModel.kt

--- app/src/main/java/com/example/pertemuan12/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/EditViewModel.kt
@@ -1,2 +1,54 @@
 package com.example.pertemuan12.viewmodel
 
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan12.modeldata.DetailSiswa
+import com.example.pertemuan12.modeldata.UIStateSiswa
+import com.example.pertemuan12.modeldata.toDataSiswa
+import com.example.pertemuan12.modeldata.toUIStateSiswa
+import com.example.pertemuan12.repositori.RepositoryDataSiswa
+import com.example.pertemuan12.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUIStateSiswa(true)
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    suspend fun editSatuSiswa(){
+        if (validasiInput(uiStateSiswa.detailSiswa)){
+            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa, uiStateSiswa.detailSiswa.toDataSiswa())
+
+            if (call.isSuccessful){
+                println("Update Sukses : ${call.message()}")
+            }else{
+                println("Update Error : ${call.errorBody()}")
+            }
+        }
+    }
+}
