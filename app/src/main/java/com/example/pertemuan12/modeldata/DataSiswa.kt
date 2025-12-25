9f7f03a1391aa6e1f8d2ef85e8f44bd4bf67013c
Lalu Fatan <lalufatan25@gmail.com>
2025-12-25 19:53:42 +0700

    menambahkan code didatasiswa.kt

--- app/src/main/java/com/example/pertemuan12/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/modeldata/DataSiswa.kt
@@ -4,22 +4,23 @@ package com.example.pertemuan12.modeldata
 import kotlinx.serialization.Serializable
 
 @Serializable
-
-data class  DataSiswa(
-    val id :Int,
-    val nama : String,
+data class DataSiswa(
+    val id: Int,
+    val nama: String,
     val alamat: String,
-    val telpon : String,
+    val telpon: String
 )
-data class  UIStateSiswa(
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid: Boolean = false
-)
-data class  DetailSiswa(
-    val id :Int = 0,
-    val nama : String = "",
-    val alamat: String = "",
-    val telpon : String= "",
 )
 
 fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
@@ -29,13 +30,14 @@ fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
     telpon = telpon
 )
 
-fun DataSiswa.toUiStatesSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
-    detailSiswa = this.toDetailSiswa(),
-    isEntryValid = isEntryValid
-)
 fun DataSiswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
     id = id,
     nama = nama,
     alamat = alamat,
     telpon = telpon
+)
+
+fun DataSiswa.toUIStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
 )
